6dbd028ccb2b117d2e2f055469aebd2a9f0c59c5
Valentine23v <1175566927@qq.com>
2017-11-20 16:41:43 +0800

    SpringBootDemo

---
+++ src/test/java/com/example/demo/web/HelloTest.java
@@ -0,0 +1,33 @@
+package com.example.demo.web;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+public class HelloTest {
+	
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setUp()throws Exception{
+		mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+		}
+	
+	@Test
+	public void hello() {
+		System.out.println("Junit");		
+	}
+	@Test
+    public void getHello() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/hello?name=小明").accept(MediaType.APPLICATION_JSON_UTF8)).andDo(print());
+    }
+
+}
